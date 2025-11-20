e9070feb2dfb09f6174745868cfe7510b6cc6e37
Aisyah Tria Putri <tria24ti@mahasiswa.pcr.ac.id>
2025-11-20 08:45:24 +0700

    push

---
+++ database/seeders/CreateFirstUser.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class CreateFirstUser extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'gatot@pcr.ac.id',
+            'password' => Hash::make('gatotkaca')
+        ]);
+    }
+}
